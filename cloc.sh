e3b4a9b96a546e227ca8ffe530b9bff4936f0f5e
Jean-Sebastien Legare <jslegare@cs.ubc.ca>
2017-04-27 13:32:40 -0700

    [cloc] counting lines of code.
      could definitely cut some fat in a few places.
    
    ------------------------------------------------------------------------------------------
    File                                                   blank        comment           code
    ------------------------------------------------------------------------------------------
    ./mon/ext/background.js                                  294            269           1334
    ./mon/ext/keyclasses.js                                  283            293            988
    ./mon/ext/pageapi/runtime.body.js.in                     218            238            982
    ./mon/ext/twitter.js                                     175            253            827
    ./mon/ext/utils.js                                       160            227            754
    ./mon/ext/certs.js                                       139            157            701
    ./mon/ext/options.js                                     117             84            633
    ./mon/ext/ui.js                                          124             71            411
    ./mon/ext/pack.js                                         65            117            403
    ./mon/ext/popup.js                                        68             24            396
    ./mon/ext/contentscript.js                                92             99            342
    ./mon/ext/options.html                                    65             21            305
    ./mon/ext/vault.js                                        68             73            296
    ./mon/ext/github.js                                       49             81            265
    ./mon/ext/outbox.js                                       63             83            208
    ./mon/ext/pageapi/runtime.globals.js.in                   25             50            142
    ./mon/ext/tests.js                                        28             31            133
    ./mon/ext/base16k.js                                      34             50            126
    ./mon/ext/stats.js                                        27             34             99
    ./mon/ext/emitter.js                                      26             50             72
    ./mon/ext/Makefile                                        18              2             46
    ./mon/ext/popup.html                                       8              1             42
    ./Makefile                                                 4              0             16
    ./mon/ext/pageapi/runtime.js.in                            1              5              6
    ./mon/ext/config.h                                         2              3              6
    ./mon/ext/pageapi/Makefile                                 1              0              4
    ./mon/ext/main.js                                          5             21              2
    ------------------------------------------------------------------------------------------
    SUM:                                                    2159           2337           9539
    ------------------------------------------------------------------------------------------

---
+++ cloc.sh
@@ -0,0 +1,102 @@
+#!/bin/bash -x 
+
+# Beeswax - Anti-Exfiltration Web Platform
+# Copyright (C) 2016  Jean-Sebastien Legare
+#
+# Beeswax is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Beeswax is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with Beeswax.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+HERE=$(cd $(dirname "$0") && pwd)
+EXT=${HERE}/mon/ext
+
+EXCLUDE_DIRS=(
+    "$HERE"/.git
+    "$HERE"/twistor_client
+    "$HERE"/sjcl
+    "$HERE"/lint
+    "$HERE"/test
+    "$EXT"/logo
+    "$EXT"/fonts
+    "$EXT"/closure-compiler
+    "$EXT"/lib
+    "$EXT"/css
+)
+
+EXCLUDE_FILES=(
+ "$HERE"/eval/README.txt
+ "$HERE"/.vimrc
+ "$HERE"/.dir-locals.el
+ "$HERE"/.hgignore
+ "$HERE"/.jshintrc
+
+ "$EXT"/utfnotes.txt
+ "$EXT"/warning19.xcf
+
+ "$EXT"/pageapi/runtime.js
+ "$EXT"/pageapi/runtime.min.js.map
+ "$EXT"/pageapi/runtime.body.js
+ "$EXT"/pageapi/runtime.min.js
+ "$EXT"/pageapi/runtime.globals.js
+
+ #"$EXT"/pageapi/Makefile
+ #"$EXT"/pageapi/runtime.body.js.in
+ #"$EXT"/options.js
+ #"$EXT"/config.h
+ #"$EXT"/background.js
+
+ #"$EXT"/base16k.js
+
+ #"$EXT"/ui.js
+ #"$EXT"/keyclasses.js
+ #"$EXT"/fonts
+ #"$EXT"/pack.js
+ #"$EXT"/popup.js
+ #"$EXT"/css
+ #"$EXT"/css/font-awesome.min.css
+ #"$EXT"/outbox.js
+ #"$EXT"/main.js
+ #"$EXT"/Makefile
+ #"$EXT"/stats.js
+ #"$EXT"/parking.js
+ #"$EXT"/tests.js
+ #"$EXT"/manifest.json
+ #"$EXT"/popup.html
+ #"$EXT"/options.html
+ #"$EXT"/certs.js
+ #"$EXT"/contentscript.js
+ #"$EXT"/vault.js
+ #./Makefile
+
+ "$EXT"/monitor.png
+ "$EXT"/sjcl.js
+ ./cloc.sh
+ ./README.md
+ "$HERE"/LICENSE
+
+ # exclude backup files
+ $(find "$HERE" -name '*~' -o -name '#*#')
+)
+
+
+
+(
+    cd "$HERE"
+    cloc --force-lang="Javascript",in --exclude-list-file=<(set +x; for x in ${EXCLUDE_FILES[@]}; do echo "$x"; done) \
+	 --exclude-dir="$(IFS=,; echo "${EXCLUDE_DIRS[*]}")" \
+	 --by-file .
+
+    cloc --force-lang="Javascript",in --exclude-list-file=<(set +x; for x in ${EXCLUDE_FILES[@]}; do echo "$x"; done) \
+	 --exclude-dir="$(IFS=,; echo "${EXCLUDE_DIRS[*]}")" \
+	 .
+)
