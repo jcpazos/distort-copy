04430f3b6375ca8d9b22d58ffa8b4684f305059d
јӗѦӤ-ֆߥ߈عဌႵ႞ᎬᏁ <jslegare@cs.ubc.ca>
2017-05-10 02:06:09 -0700

    [creds] store user information for eval in a csv.

--- eval/creds-server/creds.py
+++ eval/creds-server/creds.py
@@ -8,6 +8,11 @@
 import logging
 import logging.handlers
 import datetime
+import os.path
+import urllib
+import csv
+import tempfile
+from functools import wraps
 
 import bottle as B
 
@@ -44,6 +49,105 @@ def init_log(logger, levelname):
 
     logger.addHandler(handler)
 
+def mkdirp(dirname):
+    """ same as mkdir -p """
+    try:
+        os.makedirs(dirname)
+    except OSError, ose:
+        if ose.errno == 17:
+            pass
+        else:
+            raise
+
+def EnableCORS(allow_methods, allow_headers=None, allow_creds=False,
+               expose_headers=None, max_age=600):
+    if allow_headers is None:
+        allow_headers = ['Origin', 'Accept', 'Content-Type', 'Content-Length', 'Authorization']
+    if expose_headers is None:
+        expose_headers = ['Content-Length']
+
+    def set_preflight_response_headers(for_origin):
+        'OPTIONS response headers'
+        B.response.headers['Access-Control-Allow-Origin'] = for_origin
+        B.response.headers['Access-Control-Allow-Methods'] = ", ".join(allow_methods)
+        if allow_headers:
+            B.response.headers['Access-Control-Allow-Headers'] = allow_headers
+        if allow_creds:
+            B.response.headers['Access-Control-Allow-Credentials'] = "true"
+        B.response.headers['Access-Control-Max-Age'] = str(max_age)
+
+        # The response varies based on the source origin. Help cache appropriately.
+        B.response.headers['Vary'] = "Origin"
+
+    def set_response_headers(for_origin):
+        'extra response headers on non-OPTIONS methods'
+        B.response.headers['Access-Control-Allow-Origin'] = for_origin
+        if expose_headers:
+            B.response.headers['Access-Control-Expose-Headers'] = expose_headers
+        if allow_creds:
+            B.response.headers['Access-Control-Allow-Credentials'] = "true"
+        # The response varies based on the source origin. Help cache appropriately.
+        B.response.headers['Vary'] = "Origin"
+
+    def _cors_decorator(fn):
+        @wraps(fn)
+        def _cors_wrapper(*args, **kwargs):
+            request_origin = B.request.headers.get('Origin', None)
+
+            if B.request.method == "OPTIONS":
+                if request_origin is None:
+                    raise B.HTTPError(403, "No Origin specified on OPTIONS request")
+                set_preflight_response_headers(request_origin)
+            else:
+                if request_origin is None:
+                    return fn(*args, **kwargs)
+
+                set_response_headers(request_origin)
+                return fn(*args, **kwargs)
+        return _cors_wrapper
+    return _cors_decorator
+
+
+class CredsDB(object):
+    """
+    key value store file
+
+    instance_id,twitter_id,twitter_pass,gh_id,gh_pass,keypair_export,cert_export
+    """
+    def __init__(self, fname):
+        self.fname = fname
+        self.db = None
+        self.fieldnames = None
+        self._load()
+
+    def _mkdir(self):
+        mkdirp(os.path.dirname(self.fname))
+
+    def _load(self):
+        data = {}
+        with open(self.fname) as csvfile:
+            reader = csv.DictReader(csvfile, restval='extra')
+            for row in reader:
+                print row
+                data[row['instance_id']] = row
+            self.fieldnames = reader.fieldnames
+            self.db = data
+
+    def _save(self):
+        if not self.db:
+            return
+
+        with tempfile.NamedTemporaryFile(delete=False) as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
+            writer.writeheader()
+            for key in sorted(self.db.keys()):
+                writer.writerow(self.db[key])
+        os.rename(csvfile.name, self.fname + ".save")
+
+    def get(self, key, default=None):
+        """get the csv row"""
+        return self.db.get(key, default)
+
 class CredsApp(object):
 
     def __init__(self, opts):
@@ -51,19 +155,41 @@ class CredsApp(object):
         self.app = B.Bottle()
         self.opts = opts
 
+        self.creds_db = CredsDB(os.path.join(opts.configdir, "creds.csv"))
+        self.logs_dir = opts.logsdir
+
         self.app.route('/', method=['GET', 'OPTIONS'])(self.root)
-        self.app.route('/config/<name>', method=['GET'])(self.config)
+        self.app.route('/config/<name>', method=['OPTIONS', 'GET'])(self.config)
+        self.app.route('/logs/<name>', method=['OPTIONS', 'POST'])(self.save_logs)
+
+    @staticmethod
+    def _bork(msg, status=500):
+        raise B.HTTPResponse(status=status, body={'error': True, 'message': msg})
 
     @staticmethod
     def root():
         """ default index. noop """
         return {"name": "default"}
 
+    @EnableCORS(['GET'])
     def config(self, name):
         """ get the config bundle """
-        return B.static_file(name + ".json", root=self.opts.staticdir)
+        data = self.creds_db.get(name)
+        if not data:
+            self._bork('no such instance', status=404)
+
+    @EnableCORS(['POST'])
+    def save_logs(self, name):
+        """ clients can submit their console logs to the server to persist them """
+        mkdirp(self.logs_dir)
+        logfile = os.path.join(self.logs_dir, urllib.quote(name, safe='') + ".log")
+        with open(logfile, "a") as logfile:
+            data = B.request.body.read()
+            logfile.write(data + "\n")
+        return {}
 
 class AccessLogMiddleware(object):
+    """monkey patch bottle so that it logs each request"""
     def __init__(self, app):
         self.app = app
 
@@ -97,9 +223,12 @@ def main():
     import argparse
     parser = argparse.ArgumentParser(description='credentials server')
 
-    parser.add_argument('--staticdir', metavar="D",
-                        help='static file directory',
-                        default=".")
+    parser.add_argument('--configdir', metavar="D",
+                        help='configuration directory',
+                        default="./config")
+    parser.add_argument('--logsdir', metavar="D",
+                        help='directory to store client logs',
+                        default="./logs")
     parser.add_argument('--port', metavar='P', type=int,
                         help='port number',
                         default=60000)
